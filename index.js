bc41a282144b24118a1fc4445c4f28b978997bba
Hyuka <86443416+LokiGRDP@users.noreply.github.com>
2023-07-03 14:11:34 +0100

    + Nutrition Tracking

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ function logExercise(exerciseType, duration, sets, reps, weight) {
   console.log(`Exercise logged: Type - ${exerciseType}, Duration - ${duration}, Sets - ${sets}, Reps - ${reps}, Weight - ${weight}`);
 }
 
+// Log nutrition
+function logNutrition(mealType, foodItems, calories) {
+  // Code to handle nutrition logging, such as storing the nutrition details in a database or sending them to an API
+  console.log(`Nutrition logged: Meal Type - ${mealType}, Food Items - ${foodItems}, Calories - ${calories}`);
+}
+
 // Example usage of exercise logging
 const exerciseType1 = "Weightlifting";
 const duration1 = 60; // in minutes
@@ -35,6 +41,19 @@ const reps2 = 5;
 const weight2 = 100; // in kilograms
 
 logExercise(exerciseType2, duration2, sets2, reps2, weight2);
+
+// Example usage of nutrition logging
+const mealType1 = "Breakfast";
+const foodItems1 = ["Oatmeal", "Banana", "Almond Milk"];
+const calories1 = 350;
+
+logNutrition(mealType1, foodItems1, calories1);
+
+const mealType2 = "Lunch";
+const foodItems2 = ["Grilled Chicken Breast", "Brown Rice", "Mixed Vegetables"];
+const calories2 = 600;
+
+logNutrition(mealType2, foodItems2, calories2);
 
 // Update the step count and display periodically (e.g., every 5 minutes)
 setInterval(() => {
